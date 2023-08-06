37c7a876d5f0da71e1e0ad7cdc90f97219d1ab40
Chart Chongcharoen <chartchuo@gmail.com>
2023-08-06 15:53:00 +0700

    EP5

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,12 +1,18 @@
+// ignore_for_file: unnecessary_overrides
+
 import 'package:get/get.dart';
+import 'package:getx_tutorial/app/data/models/api_model.dart';
+import 'package:getx_tutorial/app/data/providers/api_provider.dart';
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
 
   final count = 0.obs;
+  final products = <Product>[].obs;
   @override
   void onInit() {
     super.onInit();
+    getProduct();
   }
 
   @override
@@ -17,6 +23,13 @@ class HomeController extends GetxController {
   @override
   void onClose() {
     super.onClose();
+  }
+
+  void getProduct() async {
+    var r = await Get.find<ApiProvider>().getApi();
+    var p = r?.products ?? [];
+    products.clear();
+    products.addAll(p);
   }
 
   void increment() => count.value++;
