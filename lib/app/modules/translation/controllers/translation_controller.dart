1a20c7d70d6b7720cb7244ac0ef2832b08507727
Chart Chongcharoen <chartchuo@gmail.com>
2023-09-03 10:54:31 +0700

    refactor

--- lib/app/modules/translation/controllers/translation_controller.dart
+++ lib/app/modules/translation/controllers/translation_controller.dart
@@ -1,23 +1,7 @@
 import 'package:get/get.dart';
 
 class TranslationController extends GetxController {
-  //TODO: Implement TranslationController
-
   final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
 
   void increment() => count.value++;
 }
